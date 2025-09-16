744be5407e028f1a63f2bdb885ca2a962f4a5ea3
Social Jawn <rickey.lee@socialjawn.com>
2025-09-16 12:23:20 -0400

    Video 3

--- YouAreAwesoom/ContentView.swift
+++ YouAreAwesoom/ContentView.swift
@@ -11,17 +11,27 @@ struct ContentView: View {
     @State private var message = "I am a programmer!"
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+            Spacer()
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.glassProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
