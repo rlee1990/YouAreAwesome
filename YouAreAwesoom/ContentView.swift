7ba623ee53fab6b00bf097472602e8f8b4047c5d
Social Jawn <rickey.lee@socialjawn.com>
2025-09-16 12:07:13 -0400

    Video 2

--- YouAreAwesoom/ContentView.swift
+++ YouAreAwesoom/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a programmer!"
     var body: some View {
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("Your Are Awesome!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
     }
