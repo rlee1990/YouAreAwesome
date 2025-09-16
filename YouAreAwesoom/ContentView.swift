f652ef046f27385604df0effea3a89c39df4a360
Social Jawn <rickey.lee@socialjawn.com>
2025-09-16 10:56:38 -0400

    Initial Commit

---
+++ YouAreAwesoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesoom
+//
+//  Created by rickey lee on 9/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesoom/YouAreAwesoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesoomApp.swift
+//  YouAreAwesoom
+//
+//  Created by rickey lee on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
